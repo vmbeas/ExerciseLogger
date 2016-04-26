a171e27f1ac55e36acce7282b1f6bd68a4f4d5f0
Victor B <victormbeas@gmail.com>
2016-04-26 12:00:48 -0500

    Create Visual
    Created WPF form, added labels, textboxes, submit and clear forms. Logs
    to a simple text file.

--- ExerciseLogger/MainWindow.xaml.cs
+++ ExerciseLogger/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ExerciseLogger
 {
@@ -24,5 +25,26 @@ namespace ExerciseLogger
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string output = string.Format(DateTime.Today.ToString("d") + "\r\nExercise Name:{0}\r\nWeight:{1}\r\n" +
+                                            "Reps:{2}\r\nRest Time:{3}\r\nNotes:{4}\r\n", 
+                                            nameTxt.Text, weightTxt.Text, repTxt.Text, restTxt.Text, notesTxt.Text);
+            StreamWriter file = new StreamWriter("log.txt", true);
+            file.WriteLine(output);
+            file.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTxt.Clear();
+            weightTxt.Clear();
+            repTxt.Clear();
+            restTxt.Clear();
+            notesTxt.Clear();
+        }
+
+
     }
 }
